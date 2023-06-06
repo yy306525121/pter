e857d0b1fbb44ecfdd8a39b4e613c53cdbccc1e8
yangzy <306525121@qq.com>
2023-06-06 20:52:55 +0800

    init

---
+++ src/main/java/cn/codeyang/pter/PterApplication.java
@@ -0,0 +1,13 @@
+package cn.codeyang.pter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/codeyang/pter/PterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codeyang.pter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
